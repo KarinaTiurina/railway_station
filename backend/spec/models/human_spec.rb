1a22560b8dd5e87747ed1222e6d6af923d100651
Karina Tiurina <rktiurina@gmail.com>
2018-04-06 09:46:47 +0300

    rspec: Human

---
+++ backend/spec/models/human_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Human, type: :model do
+  context 'validations check' do
+    it { should validate_presence_of :full_name }
+    it { should validate_presence_of :birth }
+    it { should validate_presence_of :phone }
+  end
+
+  # context 'references check' do
+  #   it { should belong_to :user }
+  # end
+end
